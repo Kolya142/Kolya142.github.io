242d0129c22b8057102819416fa74ac600fefde3
Kolya <87275865+Kolya142@users.noreply.github.com>
2022-08-29 13:09:01 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function getCookie(name) {
+	var cookies = document.cookie.split("; ");
+	var f = "en";
+	for (i of cookies) { 
+		if (i.split("=")[0] == name) {f = i.split("=")[1];}
+	}
+	return f
+}
+lang = getCookie("lang");
+function main() {
+    if (lang == "en") {
+        document.getElementById("hi").innerHTML = "Hello";
+    }
+    else {
+        document.getElementById("hi").innerHTML = "Привет";
+    }
+}
+function lang_button() {
+    if (lang == "en") {
+        lang = "ru";
+    }
+    else {
+        lang = "en";
+    }
+    main();
+    document.getElementById("l").value = lang;
+    document.cookie = "lang=" + lang
+}
