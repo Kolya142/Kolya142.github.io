5f5f569ec9891ce2ca4cc98c6f1da8f5fe220c0f
Gudhein3 <nikolasshevelkos@gmail.com>
2026-08-18 20:29:19 +0300

    Init :-)

---
+++ wheregp/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -rvf ~/Programming/Gudhein3.github.io/wheregp/*
+cp -rvf * .gitignore ~/Programming/Gudhein3.github.io/wheregp
+cd ~/Programming/Gudhein3.github.io/wheregp
+bash
---
+++ wheregp/script.js
@@ -0,0 +1,261 @@
+const script_s_whereGP_version = 6;
+
+const gudhein3s_key = "-----BEGIN PGP PUBLIC KEY BLOCK-----\n\nxsFNBGkJyGgBEACtLn/DpUmujh77NbESzVa3/Mt8p/uyaYL3prUP2+3rbdakFYH1\n7AnDIcbdXPAuwQFU7Rm+GkojwfhBSiXNxZKPgSoU5h/dAdia5z/hCYbtkBN0hRlI\nQILKDPl58DkVBnkvxQj7T1vYfGagIilCHTe36WUW8Z7+iPRUwBnMF6MsISNNlfEN\ndY9ZPhraQleCcqP+gmTcJrgZKrb0ksdfqKja8Scct3G0fQjl5jVB7YR3xxRoyLmx\ngHmHnaQYf2/APHAfMylkjfm15c5IA9XqRbhbBIvLvu1H9tkH5vDAQ/rop8j6WycP\nWkePp2FlxChct3vtHVMO/19ARSz2EGp+talFYm8GJET47unZUNq4T+VYLic4GeE3\n4N304mxFi++blCl98MwQuDGVcCA7ZXkvniG0mc6sJWrkONDHWUAp+gC6FoLq5ng2\neWyqbAc5NqNe7WxPo5va6NpYCJ9+eEcGjiCpisCVAPqo0OJIibw0GGL3ytzKQV90\nGBWTmnoOBM8ODtpFiUoemPpQwxuNLQZPdmj/S9ZKGWo++FGzqVFnFy2pe1ulOOtt\nWky91DxcQfaeQIJ+SUYb2C4skoKFHQyC9ho0BYt8nAne07UqthBkna7e1Ba5LTW7\n3Lzh+jnuh+73lc4xPHn0A2umHlCg5+gRjO0QdXEFCkM595CNIPH0Tmsd8QARAQAB\nzTlOaWtvbGF5IFNoZXZlbGtvIChHbnVQRyBrZXkpIDxuaWtvbGFzc2hldmVsa29z\nQGdtYWlsLmNvbT7CwY4EEwEKADgWIQRuNE9qFb0tzDVM1AQrt0D7bUrlIwUCaQnI\naAIbAwULCQgHAgYVCgkICwIEFgIDAQIeAQIXgAAKCRArt0D7bUrlI5eHD/9j/lS7\naTMfMtsK4/nAwP/X21edW9sJvzyiDmFH9HMShUtuktgQol4soYxeh4/megi/jAAv\npN2klTeVO5pjuDj6jfpHyPre0n2hQrQH41Jo8mTyKDASJbN94grkiQpcn4LYpxeD\n5NdmHWQeyDrj+XXGD689fiqtGWeQJWut+p9dhzapFaPdXBysiJp3ejXI8gLJOCJy\n2axn5LVnVhoVv9n7GkAffPreikNxm4W1DlvYlBp+D+gP3e0rw0Hf0deN2P2svq2j\nkfz8/QNsu9Z9ydiW64hK6+ZVqL+3zsfS8NBJuAlLw3Ys8yV5kZv4KiqlMMM2a/af\nGe0oyn27lrJVzfXWQDkHJUyzKSr4CDj8cvkKwfyL86Sv34emMiGQZfoje3deENNE\nC9/YgKJO6oKnSwXYt/WOq5IaP0tyV3od/anbtEWrrXsEATdTwJqswmHlixo9Hk2h\nTuNCeasp+0NXmDt/Exuo6mOQSFEzTox7Gq3qpkv0ubNQBFEjTf9252qqu23c+0Ge\nZ61ed8YmgWjHscp/WIHcNZjtC3YIEO3KKBMZyFwqfHza94etFcKDYFOTYcJWEYra\ncqYM3btVGXSRWvBEGwp8n/yrddJVwZLyK/6GIuYxrDLhIjom/FZKaMro7xpi/jK1\nAuiY2QvszYGBUz8KdV2sebGuGLTGOrHKhTe+c87BTQRpCchoARAA4tRqap+PDY+e\nu7UzR4FOdu7TYCLlYVqP1B/z48Q4ROlzlXYsZgtOS/YRUqwqsATaXs0Ty7WYVlFT\nK9egPs2oAWBaJBRpLwnzKaO8I+7hu4PQZD7+Dz5SCs/6A3FsWCdOq+qms1jxyWDp\nGvs+TAl0TQLgwh5sj575DX01bHUNJCUgjSzp+cN/A3cb6C8+2flx+CgtRu1Kc4FF\n2IqphcH5TxTmvTXeIcELo36XO2uwn83KdP6Qi0ZDrkDZYsdeTE5+f0vd0Gv1VgHh\ndjz57dNgpUAaEt49Z/vT3r9Q+jQ5p/JM2V89CB5PzWXEhGt58YKeTeB0PJVZYCCB\nZf0vkiIJF14n5dFOS/OHKVUbI+UkcGocwuY6Ir91wV61x01eDHzxH5CQyzxrCfHL\nEycrDfP5BEIybxwQlEuGe1SewMs3oIMniwg5cC53GO/zBSemNf8u0m/04MvmRh3L\nnn3NY7mGm9lrHVTL9SOj9v5CHDchEmYyzi8X3SuRe7NGN7g6ZhrjWOn/yZ/BPSGc\n3Tij5GGjVwqDmIeXl78fkgJg/ArD26YCMaBlCGxlh1GWSchp8Xd6LQWD6ifVVdp5\nldAnUIkYPRO9iTyPCuAAiMrmgnK98QGvJ1ITcCBXzZ1zrKho/Y193EQQ0wK7tXab\nU486+K5PtfrpOGg7tm9IkGGYpnxRwA8AEQEAAcLBdgQYAQoAIBYhBG40T2oVvS3M\nNUzUBCu3QPttSuUjBQJpCchoAhsMAAoJECu3QPttSuUj7SYQAITVWSxQcEj3FYbO\n5dnraBX2BsDLWcJYu6aVDcqPJKZY+gqPMYrr07ifnn8twttSM2hJ52YmtgOVGlrL\nq4GXlis+ndZRzHBI7Ss+cgj33EmMliOPH5MCXp7cAGgP7znucP8ITkiTIBdcX7YL\nHvhF1mAfsYs/dfkMhVyrg+HKIWP87OjpKP8s8/l7BUaWHsZsVXojH3LzABDewj0/\n6kAcNKDUc0bOuQlJVtbh7IUXn/3FmCtYTTY4lX5Cau4D4seTGu+Bj7teRPeDEWMz\nWoaTFqqcfCKlhLPBftnsDF9Xxp60WpSbLvQUS/eJcJFw2fqkVoW4xfrhPY/TuMIK\nQQ9z5E1Uyc98AKmwjEQOa6CkuS3oJ/D1Ff9eW9CDZiacHklJ7o7PbZU9N0d095BS\nC0biqJQpQKtkxUT06yl1Uoi0H9YLubo8IQOyhXo1ANYWxWnwdaI/PS40dgTqBZNh\njaXwXU73JX/qPb2T0j/WA5EZG9aULlfy9QkKqO6vRFqUv9VTAYkULiFKX1LXJrrI\nV9bt0Kukny/DIcOHhxrf27pPwtMqYQnSssMDv8aocoBApFGcazYOx4YmEByD6WIb\n/fPfvv2GBn+i8+NoMI8Fz1y2TnowY6PrdJ0Neb1OLM7IrnX4sDhV9zEjIhCvsqk9\nyJrylrxC4L1b6QXsYsRv4OTTAP+B\n=o+nY\n-----END PGP PUBLIC KEY BLOCK-----\n";
+
+let has_loggedin = (localStorage.getItem("mypriv") !== null);
+
+if (localStorage.getItem("contact") === null) localStorage.setItem("contact", JSON.stringify({"Gudhein3": gudhein3s_key}));
+
+async function login(a) {
+    await (async () => {
+        if (a !== undefined && has_loggedin) {alert(localization.uralg);return;}
+
+        alert(localization.this_inf_for_key);
+        const q_name = prompt(localization.enter_fn);
+        let q_email = prompt(localization.enter_em);
+        while (!validateEmail(q_email)) {
+            q_email = prompt(localization.enter_em_inv);
+        };
+        const key = await openpgp.generateKey({
+            userIDs: { name: q_name, email: q_email },
+            type: 'ecc',
+            curve: 'curve25519',
+            format: 'object'
+        });
+        const pa = key.publicKey.armor();
+        localStorage.setItem("mypriv", key.privateKey.armor());
+        localStorage.setItem("mypub", pa);
+        let contact_arr = JSON.parse(localStorage.getItem("contact"));
+        contact_arr["Me"] = pa;
+        localStorage.setItem("contact", JSON.stringify(contact_arr));
+        update_contacts_list();
+
+        has_loggedin = true;
+    })();
+}
+
+async function alogin() {
+    if (!has_loggedin) {
+        if (!confirm(localization.undlg)) return false;
+        await login(0);
+    }
+    return true;
+}
+
+if (localStorage.getItem("WGPversion") === null) {
+    if (localStorage.getItem("mypriv") !== null)
+        localStorage.setItem("WGPversion", "4");
+    else
+        localStorage.setItem("WGPversion", String(script_s_whereGP_version));
+}
+
+const user_s_whereGP_version = Number(localStorage.getItem("WGPversion"));
+
+console.log(user_s_whereGP_version);
+
+// 4 - Before version counting.
+// 5 - Added version counting. Added a reflexive "Me" contact.
+// 6 - Now you'll have my contact by default without your personal consent :-)
+
+if (!window.localization) {
+    alert("There is no localization!!!");
+}
+
+function validateEmail(email) { // https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript#46181
+    return !!(String(email)
+              .toLowerCase()
+              .match(
+                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+              ));
+};
+
+async function gk() {
+    await login(0);
+}
+
+async function cwo() {
+    out.select();
+    out.setSelectionRange(0, 9999999999);
+    navigator.clipboard.writeText(out.value);
+}
+
+async function cif() {
+    contq.value = "";
+    inp.value = "";
+    out.value = "";
+}
+
+let do_restrict_real_names = true;
+
+async function rrn() {
+    do_restrict_real_names = !do_restrict_real_names;
+    if (do_restrict_real_names)
+        _rrn.value = localization.d_rs_rl_nms;
+    else
+        _rrn.value = localization.rs_rl_nms;
+    update_contacts_list();
+}
+
+async function tlt() {
+    legal_things.hidden = !legal_things.hidden;
+    if (legal_things.hidden)
+        _tlt.value = localization.sh_legal_th;
+    else
+        _tlt.value = localization.hd_legal_th;
+}
+
+async function cek() {
+    if (prompt(localization.ch_key_cnfm) == localization.exp_tpe) {
+        const k = prompt(localization.ch_key);
+        if (k == localization.ch_key_priv_cnfm) {
+            localStorage.setItem("mypriv", inp.value);
+        }
+        if (k == localization.ch_key_pub_cnfm) {
+            localStorage.setItem("mypub", inp.value);
+        }
+    }
+}
+
+async function gsek() {
+    if (!(await alogin())) return;
+    if (prompt(localization.gt_key_cnfm) == localization.exp_tpe) {
+        out.value = localStorage.getItem("mypriv");
+    }
+}
+
+async function adb() {
+    let contact_arr = JSON.parse(localStorage.getItem("contact"));
+    contact_arr[contq.value] = inp.value;
+    try {await openpgp.readKey({armoredKey: inp.value});}
+    catch {
+        alert(localization.cki);
+        return;
+    }
+    localStorage.setItem("contact", JSON.stringify(contact_arr));
+    out.value = localization.welcome_cn + contq.value;
+    update_contacts_list();
+}
+
+async function gtk() {
+    if (!(await alogin())) return;
+    out.value = localStorage.getItem("mypub");
+}
+
+if (has_loggedin) {
+    if (user_s_whereGP_version < 5) {
+        let contact_arr = JSON.parse(localStorage.getItem("contact"));
+        contact_arr["Owner"] = localStorage.getItem("mypub"); // Some people may know two languages so i choosed English.
+        localStorage.setItem("contact", JSON.stringify(contact_arr));
+        update_contacts_list();
+    }
+
+    if (user_s_whereGP_version < 6) {
+        let contact_arr = JSON.parse(localStorage.getItem("contact"));
+        contact_arr["Kolya142"] = kolya142s_key;
+        localStorage.setItem("contact", JSON.stringify(contact_arr));
+        update_contacts_list();
+    }
+}
+
+async function update_contacts_list() {
+    if (!has_loggedin) return;
+    contacts_list.innerHTML = "";
+    let is_any = false;
+    const contacts = JSON.parse(localStorage.getItem("contact"));
+    await (async () => {
+        for (let b of Object.keys(contacts)) {
+            is_any = true;
+            {
+                const el = document.createElement('button');
+                el.className = 'button';
+                el.innerHTML = localization.del_cn_js;
+                el.onclick = () => {
+                    if(confirm(localization.do_u_want_rm_cn+b+"?")) {
+                        const p = JSON.parse(localStorage.getItem("contact"));
+                        p[b] = undefined;
+                        localStorage.setItem("contact", JSON.stringify(p));
+                        update_contacts_list();
+                    }
+                };
+                contacts_list.appendChild(el);
+            }
+            {
+                const el = document.createElement('button');
+                el.className = 'button';
+                el.innerHTML = localization.rn_cn_js;
+                el.onclick = () => {
+                    if (confirm(localization.do_u_want_rn_cn+b+"?")) {
+                        const nn = prompt(localization.ent_new_cn_nm+b);
+                        const p = JSON.parse(localStorage.getItem("contact"));
+                        p[nn] = p[b];
+                        p[b] = undefined;
+                        localStorage.setItem("contact", JSON.stringify(p));
+                        update_contacts_list();
+                    }
+                };
+                contacts_list.appendChild(el);
+            }
+            {
+                const el = document.createElement('button');
+                el.className = 'button';
+                const key = await openpgp.readKey({armoredKey: contacts[b]});
+                if (do_restrict_real_names) {
+                    el.innerText = b + " ["+localization.rstd+"]";
+                }
+                else {
+                    if (key && key.users && key.users[0] && key.users[0].userID && key.users[0].userID.userID) {
+                        el.innerText = b + " ["+key.users[0].userID.userID+"]";
+                    }
+                    else {
+                        el.innerText = b + " ["+localization.inv_cn_id+"]";
+                    }
+                }
+                el.onclick = () => {
+                    contq.value = b;
+                };
+                contacts_list.appendChild(el);
+            }
+            {
+                const el = document.createElement('br');
+                contacts_list.appendChild(el);
+            }
+        }
+    })();
+    if (!is_any) contacts_list.innerHTML = localization.no_contacts;
+}
+
+function dad() {
+    for (let i = 15; i >= 0; --i) if (!confirm(localization.dst_al_dt + " " + i)) return;
+    localStorage.clear();
+}
+
+update_contacts_list();
+
+async function en() {
+    let contact_arr = JSON.parse(localStorage.getItem("contact"));
+    if (!contq.value) {
+        alert(localization.umsc);
+        return;
+    }
+    if (contact_arr[contq.value] === undefined) {
+        alert(localization.is_not_ur_cn + contq.value);
+        return;
+    }
+    const key = await openpgp.readKey({armoredKey: contact_arr[contq.value]});
+    out.value = await openpgp.encrypt({
+        message: await openpgp.createMessage({ text: inp.value }),
+        encryptionKeys: [key]
+    });
+}
+
+async function de() {
+    if (!(await alogin())) return;
+    const key = await openpgp.readPrivateKey({armoredKey: localStorage.getItem("mypriv")});
+    const msg = await openpgp.readMessage({ armoredMessage: inp.value });
+    out.value = (await openpgp.decrypt({
+        decryptionKeys: [key],
+        message: msg
+    })).data;
+}
+
+localStorage.setItem("WGPversion", String(script_s_whereGP_version));
